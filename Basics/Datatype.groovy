3618e92df417c49b046ac3a5d46bf08b5a1b0631
gayathridevi0703 <125954337+gayathridevi0703@users.noreply.github.com>
2023-10-16 16:43:24 +0530

    commit
    This is second

---
+++ Basics/Datatype.groovy
@@ -0,0 +1,25 @@
+class Datatype{
+    static void main(String[] args){
+        int a=10;
+
+        long b=200l;
+
+        float c=50.70f;
+
+        double d=20.10e34;
+
+        BigInteger e=10000g;
+
+        BigDecimal f=200.00g;
+
+        println("int is "+a);
+        println("long is "+b);
+        println("float is "+c);
+        println("double is "+d);
+        println("BigInteger is "+e);
+        println("BigDecimal is "+f);
+     
+        
+
+    }
+}
