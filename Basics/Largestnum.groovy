10cd1e260b023f8361ce21fe71e69d4454eac37c
gayathridevi0703 <125954337+gayathridevi0703@users.noreply.github.com>
2023-10-17 14:37:27 +0530

    comm

--- Basics/Largestnum.groovy
+++ Basics/Largestnum.groovy
@@ -1,9 +1,9 @@
 class Largestnum{
     static void main(String[] args){
         //defining array
-        def num=[1,5,6,7,10,99,34,45];
+       int[] num=[1,5,6,7,10,99,34,45];
         //operator overloading
-        def largest=num.max();
+       int largest=num.max();
         println("The largest number is  "+largest);
     }
 }
