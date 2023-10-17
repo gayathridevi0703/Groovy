3be279ecf81193f6f633d07c7f482ad6a4832ee4
gayathridevi0703 <125954337+gayathridevi0703@users.noreply.github.com>
2023-10-17 15:17:53 +0530

    commit

--- Basics/Ex1.groovy
+++ Basics/Ex1.groovy
@@ -2,9 +2,12 @@
 
 class Ex1{
     static void main(String[] args){
+
+        //defining String array
         String[] names=['Gayathri','vini','vijay','gowri','kalai','nandhini'];
 
-      println("3rd element is "+names[2]);
+        //getting third element
+       println("3rd element is "+names[2]);
      
     }
 }
--- Basics/Ex3.groovy
+++ Basics/Ex3.groovy
@@ -1,7 +1,12 @@
 class Ex3{
     static void main(String[] args){
+        //first string
         String name1='Gayathri';
+
+        //second string
         String name2=' Devi';
+        
+        //concate operation
         String merge=name1+name2;
 
         println("Concate is "+merge);
