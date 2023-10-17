840266b21554fb07abb43b7c9089006376e3d472
gayathridevi0703 <125954337+gayathridevi0703@users.noreply.github.com>
2023-10-17 10:56:49 +0530

    commit

--- Basics/Largestnum.groovy
+++ Basics/Largestnum.groovy
@@ -1,6 +1,8 @@
 class Largestnum{
     static void main(String[] args){
+        //defining array
         def num=[1,5,6,7,10,99,34,45];
+        //operator overloading
         def largest=num.max();
         println("The largest number is  "+largest);
     }
--- Basics/SimpleInterest.groovy
+++ Basics/SimpleInterest.groovy
@@ -1,9 +1,12 @@
 class SimpleInterest{
     static void main(String[] args){
-
-    int p=100000;
+  //principal amount
+    int p=100000; 
+  //no of months   
     int n=10;
+  //rate of interest  
     int r=5,si;
+  // calculating simple interest 
     si=(p*n*r)/100;
     println("Simple Interest is "+si);
 
