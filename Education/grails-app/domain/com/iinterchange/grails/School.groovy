8d94f5638df9d33362e87d1ace0c140904ac4e5f
gayathridevi0703 <125954337+gayathridevi0703@users.noreply.github.com>
2023-11-01 14:31:43 +0530

    Update School.groovy

--- Education/grails-app/domain/com/iinterchange/grails/School.groovy
+++ Education/grails-app/domain/com/iinterchange/grails/School.groovy
@@ -1,13 +1,12 @@
 package com.iinterchange.grails
 
 import grails.gorm.MultiTenant
-//multitenenant combined domain class --join in database
-//persistent domain class
+
 
 class School implements MultiTenant<School>{
 
-  String courseName//domain depends on User eg:Manager
-  String  studentName//domain depends on role 
+  String courseName
+  String  studentName 
    
 }
 
